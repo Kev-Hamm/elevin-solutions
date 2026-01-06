ac3ed3c82f3e952c269775911fa9642c88da3065
Kevin <kevhamm07@gmail.com>
2026-01-06 15:42:25 -0500

    Updated nav bar

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,24 @@
 import { Component, signal } from '@angular/core';
-import { RouterLink, RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [RouterOutlet, RouterLink],
   templateUrl: './app.html',
-  styleUrl: './app.css',
+  styleUrls: ['./app.css'],
 })
 export class App {
   protected readonly title = signal('luxe-brunches');
+
   brand = 'Luxe Brunches';
+  mobileMenuOpen = false;
+
+  toggleMenu() {
+    this.mobileMenuOpen = !this.mobileMenuOpen;
+  }
+
+  closeMenu() {
+    this.mobileMenuOpen = false;
+  }
 }
