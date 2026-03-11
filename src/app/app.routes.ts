78d79679e6f5963a8f7bc5a62a15ee478f76a4fd
Kevin <kevhamm07@gmail.com>
2026-03-10 23:44:44 -0400

    Added new images

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 import { Landing } from './landing/landing';
 import { About } from './about/about';
 import { PrivacyPolicy } from './privacy-policy/privacy-policy';
+import { Gallery } from './gallery/gallery';
 
 export const routes: Routes = [
   { path: '', component: Landing },
   { path: 'about', component: About },
+  // { path: 'gallery', component: Gallery },
   { path: 'privacy-policy', component: PrivacyPolicy },
 ];
